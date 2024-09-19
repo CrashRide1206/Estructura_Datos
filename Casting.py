8042cdceef52bc1b283b10ce06e772e32e9b0b7c
Harol Ramos <168373483+CrashRide1206@users.noreply.github.com>
2024-09-19 15:46:19 -0500

    Casting

---
+++ Casting.py
@@ -0,0 +1,10 @@
+"""
+Existe una manera de definir un tipo de dato en una variable estrictamente
+como lo veremos a continuación
+"""
+x=int(8)
+y=float(9.8)
+z=str(2)
+x1=int("3")
+y1=float("4.2")
+z1=str("s1")
